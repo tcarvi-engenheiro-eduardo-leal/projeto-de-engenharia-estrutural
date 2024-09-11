0f158468a2c3c55dd7bbcac479c43ff4ed70913f
tcarvi <tcarvi@me.com>
2024-09-11 00:33:48 -0300

    Update

--- tutorial/docusaurus.config.ts
+++ tutorial/docusaurus.config.ts
@@ -60,7 +60,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/tcarvi-engenheiro-eduardo-leal/conceitos-basicos-de-engenharia-estrutural/edit/main/tutorial/',
+            'https://github.com/tcarvi-engenheiro-eduardo-leal/projeto-de-engenharia-estrutural/blob/main/tutorial/blog/',
         },
         theme: {
           customCss: './src/css/custom.css',
